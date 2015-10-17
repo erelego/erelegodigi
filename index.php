81b8a234cd1f6090cc66aab9c3fb8be0e9703ecb
ravi kumar gupta <ravi.gupta14586@gmail.com>
2015-10-17 12:25:28 +0530

    second

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	   <meta charset="utf-8">
 	   <title></title>
 	   <meta name="viewport" content="width=device-width, initial-scale=1">
-	   <!-- Latest compiled and minified CSS -->
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
